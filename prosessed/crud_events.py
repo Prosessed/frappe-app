10fdbff03c7da236b8182c104f7595493e7fdba5
Kavin <78342682+kavin0411@users.noreply.github.com>
2024-09-13 17:44:39 +0530

    feat: update expiry date on before_save docevent of batch

--- prosessed/crud_events.py
+++ prosessed/crud_events.py
@@ -44,17 +44,16 @@ def on_submit_stock_entry(doc, method=None):
                             )
 
 def before_save_batch(doc, method=None):
-    if not doc.expiry_date:
-        if doc.reference_doctype and doc.reference_doctype == "Stock Entry":
-            expiry_date, vendor_batch = frappe.db.get_value("Stock Entry Detail",
-                            {"parent":doc.reference_name, "item_code":doc.item},
-                            ["custom_expiry_date", "custom_vendor_batch"]
-                            )
+    if doc.reference_doctype and doc.reference_doctype == "Stock Entry":
+        expiry_date, vendor_batch = frappe.db.get_value("Stock Entry Detail",
+                        {"parent":doc.reference_name, "item_code":doc.item},
+                        ["custom_expiry_date", "custom_vendor_batch"]
+                        )
 
-            if expiry_date:
-                doc.expiry_date = expiry_date
+        if expiry_date:
+            doc.expiry_date = expiry_date
 
-            if vendor_batch:
-                doc.custom_vendor_batch = vendor_batch
+        if vendor_batch:
+            doc.custom_vendor_batch = vendor_batch
 
 
