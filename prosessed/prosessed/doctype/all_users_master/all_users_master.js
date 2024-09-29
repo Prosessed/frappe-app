d9df88659a1e4bdb0237f71c02c8d146b581830b
root <root@app-prod.asia-southeast1-a.c.prosessed-portal.internal>
2024-09-29 08:13:29 +0000

    hfc customization

---
+++ prosessed/prosessed/doctype/all_users_master/all_users_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Prosessed and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("All Users Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ prosessed/prosessed/doctype/quality_check/test_quality_check.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Prosessed and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQualityCheck(FrappeTestCase):
+	pass
